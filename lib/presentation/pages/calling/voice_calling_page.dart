02efeef472a58ceb5240a5bcfd93dad00126a23e
deepak-raj_frgr <deepak.raj@qburst.com>
2026-04-06 14:52:59 +0530

    back in call

--- lib/presentation/pages/calling/voice_calling_page.dart
+++ lib/presentation/pages/calling/voice_calling_page.dart
@@ -11,62 +11,86 @@ class VoiceCallingPage extends GetView<CallingController> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: controller.onScreenTap,
-      child: Scaffold(
-        body: Column(
-          children: [
-            AppBar(
-              backgroundColor: Colors.transparent,
-              title: AppText('Hello Vibess Voice Call'),
-              centerTitle: true,
-            ),
-            Expanded(
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: AppSpacings.s20),
-                    AppText(
-                      controller.user.value.name,
-                      type: AppTextType.t20sb,
-                    ),
-                    const SizedBox(height: AppSpacings.s8),
-                    Obx(() {
-                      return AppText(
-                        AppFormatter.formathhmmss(controller.duration.value),
-                        type: AppTextType.t14r,
-                      );
-                    }),
-                    const Spacer(),
-                    HVProfileImage(
-                      user: controller.user.value,
-                      size: 150,
-                      showOnlineStatus: false,
-                    ),
-                  ],
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, _) {
+        if (didPop) return;
+        onBackButtonPressed();
+      },
+      child: GestureDetector(
+        onTap: controller.onScreenTap,
+        child: Scaffold(
+          body: Column(
+            children: [
+              AppBar(
+                backgroundColor: Colors.transparent,
+                title: AppText('Hello Vibess Voice Call'),
+                centerTitle: true,
+              ),
+              Expanded(
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: AppSpacings.s20),
+                      AppText(
+                        controller.user.value.name,
+                        type: AppTextType.t20sb,
+                      ),
+                      const SizedBox(height: AppSpacings.s8),
+                      Obx(() {
+                        return AppText(
+                          AppFormatter.formathhmmss(controller.duration.value),
+                          type: AppTextType.t14r,
+                        );
+                      }),
+                      const Spacer(),
+                      HVProfileImage(
+                        user: controller.user.value,
+                        size: 150,
+                        showOnlineStatus: false,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Spacer(),
-            SizedBox(height: controller.bottomSheetMaximumHeight),
-          ],
+              Spacer(),
+              SizedBox(height: controller.bottomSheetMaximumHeight),
+            ],
+          ),
+          bottomSheet: Obx(() {
+            return CallControlButtonsBottomSheet(
+              controller: controller.bottomSheetController,
+              bottomSheetMaximumSize: controller.bottomSheetMaximumSize.value,
+              bottomSheetInitialSize: controller.bottomSheetInitialSize,
+              bottomSheetMaximumHeight: controller.bottomSheetMaximumHeight,
+              onVolumeTap: controller.onVolumeTap,
+              onCallEndTap: controller.onCallEndTap,
+              onMicTap: controller.onMicTap,
+              loudSpeakerOn: controller.loudSpeakerOn.value,
+              micOn: controller.micOn.value,
+            );
+          }),
         ),
-        bottomSheet: Obx(() {
-          return CallControlButtonsBottomSheet(
-            controller: controller.bottomSheetController,
-            bottomSheetMaximumSize: controller.bottomSheetMaximumSize.value,
-            bottomSheetInitialSize: controller.bottomSheetInitialSize,
-            bottomSheetMaximumHeight: controller.bottomSheetMaximumHeight,
-            onVolumeTap: controller.onVolumeTap,
-            onCallEndTap: controller.onCallEndTap,
-            onMicTap: controller.onMicTap,
-            loudSpeakerOn: controller.loudSpeakerOn.value,
-            micOn: controller.micOn.value,
-          );
-        }),
       ),
+    );
+  }
+
+  void onBackButtonPressed() {
+    AppDialog.showAlertDialog(
+      negativeText: 'Yes, Disconnect',
+      positiveText: 'Cancel',
+      title:
+          'This will disconnect the ongoing call. Are you sure want to end the call ?',
+      textHeight: 50,
+      positiveOnPressed: () {
+        Get.back();
+      },
+      negativeOnPressed: () {
+        Get.back();
+        controller.onCallEndTap();
+      },
     );
   }
 }
